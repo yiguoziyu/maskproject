500ecb8a54d40e77bfca1c3369b98104eb26f65b
yiguoziyu <1197044274@qq.com>
2020-09-23 11:26:33 +0800

    增加字段

--- mask/src/main/java/com/ljj/lettercircle/model/CouponAutoGifBean.java
+++ mask/src/main/java/com/ljj/lettercircle/model/CouponAutoGifBean.java
@@ -1,10 +1,45 @@
 package com.ljj.lettercircle.model;
 
-public class CouponAutoGifBean {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class CouponAutoGifBean implements Parcelable {
     private int coupon_num;
     private String sum;
     private String title;
     private String tip;
+
+    protected CouponAutoGifBean(Parcel in) {
+        coupon_num = in.readInt();
+        sum = in.readString();
+        title = in.readString();
+        tip = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(coupon_num);
+        dest.writeString(sum);
+        dest.writeString(title);
+        dest.writeString(tip);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<CouponAutoGifBean> CREATOR = new Creator<CouponAutoGifBean>() {
+        @Override
+        public CouponAutoGifBean createFromParcel(Parcel in) {
+            return new CouponAutoGifBean(in);
+        }
+
+        @Override
+        public CouponAutoGifBean[] newArray(int size) {
+            return new CouponAutoGifBean[size];
+        }
+    };
 
     public int getCoupon_num() {
         return coupon_num;
